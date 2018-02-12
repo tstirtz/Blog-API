b81b9ff35238cbb6d115f4641ca3954409dd9a25
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-12 10:59:13 -0800

    Add runServer and closeServer functions for testing

--- server.js
+++ server.js
@@ -11,6 +11,36 @@ app.get('/', (req, res) => {
 
 app.use('/blog-posts', blogPostsRouter);
 
-app.listen(8000, function(){
-    console.log('Listening on port ' + 8000);
-});
+let server;
+
+function runServer(){
+    const port = process.env.PORT || 8080;
+    return new Promise ((resolve, reject) => {
+        //on resolve run server
+        server = app.listen('port', () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+function closeServer(){
+    //when called close server
+    return new Promise ((resolve, reject) => {
+        console.log("Closing server");
+        server.close(err => {
+            if(err){
+                reject(err);
+
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if(require.main === module){
+    runServer().catch(err => console.error(err));
+}
